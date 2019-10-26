4d98bb1d3078b839b785c2828cfddc474920a7d8
unknown <f.barajas.arnau@gmail.com>
2019-10-26 20:02:09 +0100

    10th commit

--- setup.sh
+++ setup.sh
@@ -1,24 +1,12 @@
-<<<<<<< HEAD
 mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
-email = \"email@website.com\"\n\
+email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
-=======
-mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"email@website.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
->>>>>>> 33a962bcea60f5ec8cd27ac3ef64df461ef3617c
 " > ~/.streamlit/config.toml
